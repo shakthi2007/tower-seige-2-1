79bf2ecf5a413db7abad5c9e8d114aa9ec5fd636
shakthi2007 <63248034+shakthi2007@users.noreply.github.com>
2020-06-25 11:20:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+function setup() {
+  createCanvas(2000,800);
+  
+ 
+}
+
+function draw() {
+  background("black");
+  //bottom part1
+  createSprite(600,400,20,40); 
+  createSprite(622,400,20,40);
+  createSprite(644,400,20,40);
+  createSprite(666,400,20,40);
+  createSprite(688,400,20,40);
+  createSprite(710,400,20,40);
+  createSprite(732,400,20,40);
+  // 3rd last part
+  createSprite(622,356,20,40);
+  createSprite(644,356,20,40);
+  createSprite(666,356,20,40);
+  createSprite(688,356,20,40);
+  createSprite(710,356,20,40);
+  // 2nd last part
+  createSprite(644,314,20,40);
+  createSprite(666,314,20,40);
+  createSprite(688,314,20,40);
+  // top part
+  createSprite(666,270,20,40);
+  // bootom2nd part
+  createSprite(800,200,20,40);
+  createSprite(822,200,20,40);
+  createSprite(844,200,20,40);
+  createSprite(866,200,20,40);
+  createSprite(888,200,20,40);
+  // 2nd last part
+  createSprite(822,156,20,40);
+  createSprite(844,156,20,40);
+  createSprite(866,156,20,40);
+  // top part
+  createSprite(844,114,20,40);
+
+  // box
+  box=createSprite(200,260,40,40);
+ drawSprites()
+
+}
+ 
+
+
+
+   
